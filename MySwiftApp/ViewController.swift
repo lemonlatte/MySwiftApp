b271968be4c6eff837a287c4306e6e0d3f8c97f1
Jim Yeh <lemonlatte@gmail.com>
2014-06-08 20:00:02 +0800

    Initial Commit

---
+++ MySwiftApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MySwiftApp
+//
+//  Created by Yeh, Chang-Ching on 6/8/14.
+//  Copyright (c) 2014 Yeh, Chang-Ching. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
